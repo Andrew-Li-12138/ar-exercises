00c60013f8c52f63837fbfb9d1ea18db37a7518f
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-10-19 21:24:00 -0400

    find store by id and assign it to instance variable. update store name.

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -2,6 +2,11 @@ require_relative '../setup'
 require_relative './exercise_1'
 
 puts "Exercise 2"
-puts "----------"
+puts "----------" 
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "GTA"
+@store1.save
