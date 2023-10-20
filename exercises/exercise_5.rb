5c2852f3318a6f90758590fdf48a25a2439d8595
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-10-19 22:23:43 -0400

    Calculations of revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@stores = Store.all
+puts "annual_total:#{@stores.sum(:annual_revenue)}"
+puts "annual_average:#{@stores.average(:annual_revenue)}"
+puts "number of stores which sell more than 1M: #{@stores.where('annual_revenue >= ?', 1000000).count}"
