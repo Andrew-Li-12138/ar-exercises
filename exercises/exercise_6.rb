b1c5b417b5d29ebc06ce023ee1f7c3b02f4caa88
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-10-19 22:58:15 -0400

    One-to-many association and create employees data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,25 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+  has_many :employees
+end
+
+class Employee 
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Talia", last_name: "Gladys", hourly_rate: 60)
+@store1.employees.create(first_name: "Asuran", last_name: "Zala", hourly_rate: 65)
+@store1.employees.create(first_name: "Meyrin", last_name: "Hawke", hourly_rate: 32)
+@store1.employees.create(first_name: "Shinn", last_name: "Asuka", hourly_rate: 62)
+
+@store2.employees.create(first_name: "Kira", last_name: "Yamato", hourly_rate: 66)
+@store2.employees.create(first_name: "Lacus", last_name: "Clyne", hourly_rate: 70)
+@store2.employees.create(first_name: "Murrue", last_name: "Ramius", hourly_rate: 45)
+@store2.employees.create(first_name: "Mu", last_name: "Flaga", hourly_rate: 55)
+
+
+
+
+
